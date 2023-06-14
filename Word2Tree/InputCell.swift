fbf59e3a9219aa83c850c275b52e84f978e73d81
0yama12to16 <touya121216@gmail.com>
2023-06-14 10:28:13 +0900

    split the view into inputView and tableView

--- Word2Tree/InputCell.swift
+++ Word2Tree/InputCell.swift
@@ -1,26 +1,19 @@
 //
-//  CustomTableViewCell.swift
+//  InputCell.swift
 //  Word2Tree
 //
-//  Created by ToyaOyama on 2023/06/11.
+//  Created by ToyaOyama on 2023/06/14.
 //
 
 import UIKit
 
-class CustomTableViewCell: UITableViewCell {
+class InputCell: UITableViewCell {
 
-    @IBOutlet var content: UILabel!
-    @IBOutlet var name: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    @IBAction func buttonTapped(_ sender: Any) {
-        content.text = "ボタンが押されました。"
-    }
-    
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
--- Word2Tree/ViewController.swift
+++ Word2Tree/ViewController.swift
@@ -17,8 +17,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         view.addSubview(tableView)
+        let inputView = UITableView(frame:CGRect(x: 0, y:view.bounds.height-200, width:view.bounds.width, height: 200), style: .plain)
+        self.view.addSubview(inputView)
         tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         tableView.rowHeight = 200
+        inputView.register(UINib(nibName: "InputCell", bundle: nil), forCellReuseIdentifier: "InputCell")
+        inputView.rowHeight = 200
+        
         // Do any additional setup after loading the view.
     }
     
@@ -26,8 +31,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return item.count
     }
     
-    
-    let item = ["Cell 3","Cell 4","Cell 5"]
+    let item = ["Cell 3","Cell 4","Cell 5","",""]
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
@@ -35,6 +39,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    
+    
+    func inputView(_ inputView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func inputView(_ inputView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = inputView.dequeueReusableCell(withIdentifier: "InputCell", for: indexPath) as! InputCell
+        return cell
+    }
 
     
     
