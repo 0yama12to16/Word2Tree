3516456a7472bd8d81356be98ee8ebeacdd734ff
0yama12to16 <touya121216@gmail.com>
2023-07-04 03:10:59 +0900

    (additional Fix is required) implemented the code for moving that is conducted when keyboard is opened(in the middle) resize inputCell, textField in it and the font of text in the textField. resize subview containing the inputCell corresponding the size of the cell.

--- Word2Tree/ViewController.swift
+++ Word2Tree/ViewController.swift
@@ -18,22 +18,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView1 = UITableView(frame:CGRect(x: 0, y:0, width:view.bounds.width, height: view.bounds.height-350), style: .plain)
+        tableView1 = UITableView(frame:CGRect(x: 0, y:0, width:view.bounds.width, height: view.bounds.height-200), style: .plain)
         tableView1.delegate = self
         tableView1.dataSource = self
         view.addSubview(tableView1)
         tableView1.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
         tableView1.rowHeight = 200
         
-        tableView2 = UITableView(frame:CGRect(x: 0, y:view.bounds.height-350, width:view.bounds.width, height: 400), style: .plain)
+        tableView2 = UITableView(frame:CGRect(x: 0, y:view.bounds.height-200, width:view.bounds.width, height: 200), style: .plain)
         tableView2.delegate = self
         tableView2.dataSource = self
         view.addSubview(tableView2)
         tableView2.register(UINib(nibName: "InputCell", bundle: nil), forCellReuseIdentifier: "inputCell")
-        tableView2.rowHeight = 350
+        tableView2.rowHeight = 200
         
+        NotificationCenter.default.addObserver(self, selector: #selector(openKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(closeKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         // Do any additional setup after loading the view.
     }
+    
+    @objc func openKeyboard(notification: Notification) {
+        if let keyboardRect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue{
+            self.tableView2.frame.origin.y -= keyboardRect.height
+        }
+    }
+    
+    @objc func closeKeyboard(notification: Notification) {
+        self.tableView2.frame.origin.y = view.bounds.height-200
+    }
+        
+    
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView == tableView1{
@@ -53,15 +68,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 customCell.content.text = item[indexPath.row][1]
                 customCell.name.text = item[indexPath.row][0]
             }
+            
+            var cellSelectedBgView1 = UIView()
+            cellSelectedBgView1.backgroundColor = UIColor.white
+            cell.selectedBackgroundView = cellSelectedBgView1
+            
         } else if tableView == tableView2 {
             cell = tableView.dequeueReusableCell(withIdentifier: "inputCell", for: indexPath) as! InputCell
             if let inputCell = cell as? InputCell {
                 inputCell.superView = self
             }
             
-            var cellSelectedBgView = UIView()
-            cellSelectedBgView.backgroundColor = UIColor.white
-            cell.selectedBackgroundView = cellSelectedBgView
+            var cellSelectedBgView2 = UIView()
+            cellSelectedBgView2.backgroundColor = UIColor.white
+            cell.selectedBackgroundView = cellSelectedBgView2
             
         }
         return cell
